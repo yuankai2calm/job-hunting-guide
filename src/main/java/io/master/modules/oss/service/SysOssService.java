a3cd70bf282c34d6505e28941dfb95ba1a4af63d
ykai <yuankai2calm@gmail.com>
2019-05-23 22:13:51 +0800

    【初始化项目】

---
+++ src/main/java/io/master/modules/oss/service/SysOssService.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2016-2019 人人开源 All rights reserved.
+ *
+ * https://www.renren.io
+ *
+ * 版权所有，侵权必究！
+ */
+
+package io.master.modules.oss.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import io.master.common.utils.PageUtils;
+import io.master.modules.oss.entity.SysOssEntity;
+
+import java.util.Map;
+
+/**
+ * 文件上传
+ *
+ * @author Mark sunlightcs@gmail.com
+ */
+public interface SysOssService extends IService<SysOssEntity> {
+
+	PageUtils queryPage(Map<String, Object> params);
+}
